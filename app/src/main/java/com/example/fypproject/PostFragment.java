926f99bbbc66f9223d491d851dc78dcc9db9526b
Adminstrator <limjh-wm18@student.tarc.edu.my>
2022-12-05 19:19:41 +0800

    first prototype

--- app/src/main/java/com/example/fypproject/PostFragment.java
+++ app/src/main/java/com/example/fypproject/PostFragment.java
@@ -1,32 +1,45 @@
 package com.example.fypproject;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import com.example.fypproject.databinding.FragmentPostBinding;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+
 
 
 public class PostFragment extends Fragment {
-
+    FragmentPostBinding binding;
 
 
     public PostFragment() {
         // Required empty public constructor
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_post,container,false);
+        Button btnPost = (Button) view.findViewById(R.id.postButton);
+
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UserMainPost.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false);
+        return view;
     }
 }
